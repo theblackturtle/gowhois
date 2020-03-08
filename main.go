fd8b2f88050479974a9f5d57c15d4e02687aef17
theblackturtle <tuan.tv1966@sinhvien.hoasen.edu.vn>
2020-03-08 10:51:34 +0700

    init

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/domainr/whois"
+	whoisparser "github.com/likexian/whois-parser-go"
+	"os"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		line := strings.TrimSpace(sc.Text())
+		if line == "" {
+			continue
+		}
+		request, err := whois.NewRequest(line)
+		if err != nil {
+			printError(err)
+			continue
+		}
+		response, err := whois.DefaultClient.Fetch(request)
+		if err != nil {
+			printError(err)
+			continue
+		}
+		result, err := whoisparser.Parse(response.String())
+		if err != nil {
+			printError(err)
+			continue
+		}
+		j, err := json.Marshal(result)
+		if err != nil {
+			printError(err)
+			continue
+		}
+		fmt.Println(string(j))
+	}
+}
+
+func printError(e error) {
+	fmt.Fprintf(os.Stderr, e.Error()+"\n")
+}
