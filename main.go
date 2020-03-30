72ad1f7f3179ab8b52f1aee525585e5d70720455
theblackturtle <tuan.tv1966@sinhvien.hoasen.edu.vn>
2020-03-30 22:18:02 +0700

    fix

--- main.go
+++ main.go
@@ -3,49 +3,66 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
+	whoisparser "github.com/likexian/whois-parser-go"
 	"net/url"
 	"os"
 	"os/exec"
 	"strings"
-
-	whoisparser "github.com/likexian/whois-parser-go"
 )
 
 func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	for sc.Scan() {
-		line := strings.TrimSpace(sc.Text())
-		if line == "" {
-			continue
-		}
-
-		var domain string
-		domain = line
-		if strings.HasPrefix(line, "http") {
-			u, err := url.Parse(line)
-			if err != nil {
-				domain = u.Hostname()
+	flag.Parse()
+	var domains []string
+	if flag.NArg() > 0 {
+		domains = append(domains, flag.Args()...)
+	} else {
+		sc := bufio.NewScanner(os.Stdin)
+		for sc.Scan() {
+			line := strings.TrimSpace(sc.Text())
+			if line == "" {
+				continue
 			}
+			domains = append(domains, line)
 		}
-
-		out, err := exec.Command("whois", domain).Output()
-		if err != nil {
-			printError("Whois error: " + err.Error())
-			continue
-		}
-		result, err := whoisparser.Parse(string(out))
-		if err != nil {
-			printError("Parse error: " + err.Error())
-			continue
-		}
-		j, err := json.Marshal(result)
-		if err != nil {
-			printError("Marshal error: " + err.Error())
-			continue
-		}
-		fmt.Println(string(j))
 	}
+
+	if len(domains) == 0 {
+		printError("No domains")
+		os.Exit(1)
+	}
+
+	for _, domain := range domains {
+		whois(domain)
+	}
+}
+
+func whois(domain string) {
+	if strings.HasPrefix(domain, "http") {
+		u, err := url.Parse(domain)
+		if err != nil {
+			printError("Parse URL error: " + err.Error())
+			return
+		}
+		domain = u.Hostname()
+	}
+	out, err := exec.Command("whois", domain).Output()
+	if err != nil {
+		printError("Exec command error: " + err.Error())
+		return
+	}
+	result, err := whoisparser.Parse(string(out))
+	if err != nil {
+		printError("Parse error: " + err.Error())
+		return
+	}
+	j, err := json.Marshal(result)
+	if err != nil {
+		printError("Marshal error: " + err.Error())
+		return
+	}
+	fmt.Println(string(j))
 }
 
 func printError(s string) {
